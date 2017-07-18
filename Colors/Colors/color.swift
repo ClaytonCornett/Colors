449391e33d8b563eeacac85b2667fe3b3db68b3c
Clayton Cornett <claytoncornett@claytons-mbp.attlocal.net>
2017-07-18 15:38:23 -0500

    Finished

---
+++ Colors/Colors/color.swift
@@ -0,0 +1,15 @@
+//
+//  color.swift
+//  Colors
+//
+//  Created by Clayton Cornett on 7/18/17.
+//  Copyright © 2017 Clayton Cornett. All rights reserved.
+//
+
+import UIKit
+
+struct Color{
+    
+    let name: String
+    let uiColor: UIColor
+}
